e4856ba8ba9081bf5ba20814f7744f08182c37dc
Hugo Lopes <hugo.rodrigues.lopes@gmail.com>
2023-12-09 00:22:19 +0000

    add shipping cost

--- server/server.js
+++ server/server.js
@@ -32,6 +32,51 @@ app.post('/checkout', async (req, res, next) => {
 
   try {
     const session = await stripe.checkout.sessions.create({
+      shipping_address_collection: {
+        allowed_countries: ['US', 'CA'],
+      },
+      shipping_options: [
+        {
+          shipping_rate_data: {
+            type: 'fixed_amount',
+            fixed_amount: {
+              amount: 0,
+              currency: 'usd',
+            },
+            display_name: 'Free shipping',
+            delivery_estimate: {
+              minimum: {
+                unit: 'business_day',
+                value: 5,
+              },
+              maximum: {
+                unit: 'business_day',
+                value: 7,
+              },
+            },
+          },
+        },
+        {
+          shipping_rate_data: {
+            type: 'fixed_amount',
+            fixed_amount: {
+              amount: 1500,
+              currency: 'usd',
+            },
+            display_name: 'Next day air',
+            delivery_estimate: {
+              minimum: {
+                unit: 'business_day',
+                value: 1,
+              },
+              maximum: {
+                unit: 'business_day',
+                value: 1,
+              },
+            },
+          },
+        },
+      ],
       line_items: items,
       mode: 'payment',
       success_url: `${YOUR_DOMAIN}/success.html`,
